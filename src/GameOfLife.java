3dd0e8f158e5f07bfd1b3c5bfce6b511701f334c
sArtexx <volk_sik@mail.ru>
2017-09-11 19:17:08 +0300

    Initial commit

---
+++ src/GameOfLife.java
@@ -0,0 +1,164 @@
+/*
+ * Лабораторная работа № 1 по дисциплине "Интеллектуальные системы".
+ * Проектирование реакивных интеллектуальныхх агентов. Игра "Жизнь".
+ * 
+ * Задание: реализовать приложение, которое поледовательно отображает состояние
+ * клеточной модели "Игра жизнь". Исследовать возможные конфигурации игры, которые
+ * переходят в устойчивое состояние, вымирают, либо возвращаюся в исходное
+ * состояние.
+ * 
+ * Выполнил студент 443 группы Симоненко Игнат Константинович.
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import java.util.Random;
+
+public class GameOfLife {
+	private final static String NAME_OF_GAME = "Игра 'Жизнь'. Симоненко И.К., 443 группа.";
+	private final static int START_LOCATION = 200;
+	private final static int LIFE_SIZE = 50;
+	private final static int POINT_RADIUS = 10;
+	private final static int FIELD_SIZE = (LIFE_SIZE + 1) * POINT_RADIUS;
+	private final static int BTN_PANEL_HEIGHT = 58;
+	private static boolean[][] lifeGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
+	private static boolean[][] nextGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
+	private static boolean goNextGeneration = false;
+	private static int showDelay = 200;
+	private static int currentGeneration = 0;
+	private static Canvas canvasPanel;
+	private static JLabel curGenerationLabel = new JLabel("Текущее поколение: " 
+			+ currentGeneration);
+	private static Random random = new Random();
+	
+	 //Отрисовка Canvas
+	@SuppressWarnings("serial")
+	private static class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                for (int y = 0; y < LIFE_SIZE; y++) {
+                    if (lifeGeneration[x][y]) {
+                        g.fillOval(x * POINT_RADIUS, y * POINT_RADIUS, 
+                        		POINT_RADIUS, POINT_RADIUS);
+                    }
+                }
+            }
+        }
+    }
+    
+    //Случайное заполнение клеток
+	private static class FillButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                for (int y = 0; y < LIFE_SIZE; y++) {
+                    lifeGeneration[x][y] = random.nextBoolean();
+                }
+            }
+            currentGeneration = 0;
+            curGenerationLabel.setText("Текущее поколение: " 
+        								+ currentGeneration);
+            canvasPanel.repaint();
+        }
+    }
+	    
+	public static void main (String[] args) {
+		JFrame frame = new JFrame(NAME_OF_GAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(FIELD_SIZE, FIELD_SIZE + BTN_PANEL_HEIGHT);
+        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setResizable(false);
+ 
+        canvasPanel = new Canvas();
+        canvasPanel.setBackground(Color.white);
+ 
+        JButton fillButton = new JButton("Заполнить");
+        fillButton.addActionListener(new FillButtonListener());
+ 
+        JButton stepButton = new JButton("Шаг");
+        stepButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                processOfLife();
+                canvasPanel.repaint();
+            }
+        });
+ 
+        final JButton goButton = new JButton("Пуск");
+        goButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                goNextGeneration = !goNextGeneration;
+                goButton.setText(goNextGeneration ? "Стоп" : "Пуск");
+            }
+        });
+        
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(fillButton);
+        btnPanel.add(stepButton);
+        btnPanel.add(goButton);
+        btnPanel.add(curGenerationLabel);
+ 
+        frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, btnPanel);
+        frame.setVisible(true);
+
+        while (true) {
+            if (goNextGeneration) {
+                processOfLife();
+                canvasPanel.repaint();
+                try {
+                    Thread.sleep(showDelay);
+                } catch (InterruptedException e) { 
+                	e.printStackTrace(); 
+                }
+            }
+        }
+	}
+ 
+    //Количество соседей
+	private static int countNeighbors(int x, int y) {
+        int count = 0;
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 2; dy++) {
+                int nX = x + dx;
+                int nY = y + dy;
+                nX = (nX < 0) ? LIFE_SIZE - 1 : nX;
+                nY = (nY < 0) ? LIFE_SIZE - 1 : nY;
+                nX = (nX > LIFE_SIZE - 1) ? 0 : nX;
+                nY = (nY > LIFE_SIZE - 1) ? 0 : nY;
+                count += (lifeGeneration[nX][nY]) ? 1 : 0;
+            }
+        }
+        if (lifeGeneration[x][y]) { 
+        	--count; 
+        }
+        return count;
+    }
+ 
+	private static void processOfLife() {
+        for (int x = 0; x < LIFE_SIZE; x++) {
+            for (int y = 0; y < LIFE_SIZE; y++) {
+                int count = countNeighbors(x, y);
+                nextGeneration[x][y] = lifeGeneration[x][y];
+                //Если три клетки рядом живы - клетка рождается
+                nextGeneration[x][y] = (count == 3) 
+                						? true 
+                						: nextGeneration[x][y];
+                //Если меньше двух или больше трех рядом живы - клетка умирает
+                nextGeneration[x][y] = ((count < 2) || (count > 3)) 
+                						? false 
+                						: nextGeneration[x][y];
+            }
+        }
+        for (int x = 0; x < LIFE_SIZE; x++) {
+            System.arraycopy(nextGeneration[x], 0, lifeGeneration[x], 0, LIFE_SIZE);
+        }
+        
+        ++currentGeneration;
+        curGenerationLabel.setText("Текущее поколение: " 
+    								+ currentGeneration);
+    }
+}
